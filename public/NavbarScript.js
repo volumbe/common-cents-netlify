bd07235568159d58ee4c6acbba8e0db5da23eab3
volumbe <olumbe@seas.upenn.edu>
2021-02-28 23:57:05 -0500

    netlify redirects

--- public/NavbarScript.js
+++ public/NavbarScript.js
@@ -1,13 +1,13 @@
-console.log("Public Test 2")
+console.log("Public Test 2");
 
 document.querySelectorAll('.tabs li').forEach(tab => {
   tab.addEventListener('click', () => {
     document.querySelectorAll('.tabs li').forEach(item => item.classList.remove('is-active'));
     tab.classList.add('is-active');
-    const tabCzontentBoxes = document.querySelectorAll('#tab-content > div');
+    const tabContentBoxes = document.querySelectorAll('#tab-content > div');
     const target = tab.dataset.target;
     // console.log(target);
-    tabCzontentBoxes.forEach(box => {
+    tabContentBoxes.forEach(box => {
       if (box.getAttribute('id') === target) {
         box.classList.remove('is-hidden');
       } else {
