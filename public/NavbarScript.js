03bc9ea6228a72ddea09a5f6c3d7908bce29269c
volumbe <olumbe@seas.upenn.edu>
2021-03-01 00:12:18 -0500

    Revert "netlify redirects"
    This reverts commit bd07235568159d58ee4c6acbba8e0db5da23eab3.

--- public/NavbarScript.js
+++ public/NavbarScript.js
@@ -1,13 +1,13 @@
-console.log("Public Test 2");
+console.log("Public Test 2")
 
 document.querySelectorAll('.tabs li').forEach(tab => {
   tab.addEventListener('click', () => {
     document.querySelectorAll('.tabs li').forEach(item => item.classList.remove('is-active'));
     tab.classList.add('is-active');
-    const tabContentBoxes = document.querySelectorAll('#tab-content > div');
+    const tabCzontentBoxes = document.querySelectorAll('#tab-content > div');
     const target = tab.dataset.target;
     // console.log(target);
-    tabContentBoxes.forEach(box => {
+    tabCzontentBoxes.forEach(box => {
       if (box.getAttribute('id') === target) {
         box.classList.remove('is-hidden');
       } else {
